4d9fe8c6e8a1cdcc73b8049a608e5965410876f3
PrinceHamzaYT-786 <145461712+PrinceHamzaYT-786@users.noreply.github.com>
2024-01-23 10:40:59 +0500

    Update prince.py

--- prince.py
+++ prince.py
@@ -1,1 +1,21 @@
-print(' COMMAND OFF DURING BOARD EXAMS ')
+import random
+import os
+P = '\x1b[1;97m' # PUTIH
+M = '\x1b[1;91m' # MERAH:RED
+H = '\x1b[1;92m' # HIJAU:GREEN
+K = '\x1b[1;93m' # KUNING:YELLOW
+B = '\x1b[1;94m' # BIRU:BLUE
+U = '\x1b[1;95m' # UNGU:PINK
+O = '\x1b[1;96m' # BIRU MUDA: Light Green
+N = '\x1b[0m'    # WARNA MATI: Normal Colour
+A = '\x1b[1;90m' # WARNA ABU ABU:Light Black
+BN = '\x1b[1;107m' # BELAKANG PUTIH
+BBL = '\x1b[1;106m' # BELAKANG BIRU LANGIT
+BP = '\x1b[1;105m' # BELAKANG PINK
+BB = '\x1b[1;104m' # BELAKANG BIRU
+BK = '\x1b[1;103m' # BELAKANG KUNING
+BH = '\x1b[1;102m' # BELAKANG HIJAU
+BM = '\x1b[1;101m' # BELAJANG MERAH
+BA = '\x1b[1;100m' # BELAKANG ABU ABU
+random_colours = random.choice([P,M,H,K,B,U,O,N,A,BN,BBL,BP,BB,BK,BH,BM,BA])
+print(f'{random_colours} COMMAND OFF DURING BOARD EXAMS ')
